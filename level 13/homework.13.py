ec682b2461b7154e1effcf4fe4fb4da84bc6befb
dethworks <lukashukvani1@gmail.com>
2024-10-19 13:02:51 +0400

    second commit

---
+++ level 13/homework.13.py
@@ -0,0 +1,9 @@
+                              #1
+for i in "NAME":
+        print(i)
+#თითოეული ასო იდივიდუალურად დაპრინტა
+                               #2
+
+age=int(input("Enter your age: "))
+
+print("You are older than 10" if age>=10 else "You are older than 10")

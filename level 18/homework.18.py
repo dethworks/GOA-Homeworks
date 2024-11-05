37d00b54cd679b7ab59d2b9b4f5e7c085fecc5ac
dethworks <lukashukvani1@gmail.com>
2024-11-05 21:49:47 +0400

    second commit

---
+++ level 18/homework.18.py
@@ -0,0 +1,43 @@
+                         #1
+arr1 = ["apple", "banana", "pear"]
+
+print(arr1[0])
+print(arr1[2])
+                          #2
+
+arr2 = [1,2,3,4,5,6,7,8,9,10]
+
+print(arr2[0])
+print(arr2[4])
+print(arr2[9])
+                           #3
+arr3 = ["wolf", "dog", "cat"]
+
+
+animal=["lion"]
+
+print(arr3[0],arr3[1],arr3[2], animal[0])
+                           #4
+movies = ["Rambo 1", "Cobra", "Rocky", "Over the top"]
+
+print(movies[-1])
+                          #5
+numbers1 = [10, 20, 30, 40, 50, 60]
+
+print(numbers1[4:6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

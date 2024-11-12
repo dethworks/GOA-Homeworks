ffc68b32d31e12165f5b8288c337082bf7fa9379
dethworks <lukashukvani1@gmail.com>
2024-11-12 21:58:52 +0400

    second commit

---
+++ level 20/homework.20.py
@@ -0,0 +1,59 @@
+                           #1
+
+number1 = [1,4,88,90,43,2,123]
+
+result = max(number1)
+
+print(result)
+
+                          #2
+
+                         #3
+
+abc = ["a","b","v","g"]
+numb = [1,3,5,7,8]
+
+
+print(abc+numb)
+                         #4
+for space in range(4):
+    print(" " * space + '*' * 6)
+
+                        #5
+
+age = int(input("Enter your age: "))
+
+if age > 12:
+    print("You are over 12")
+else:
+    print("You are not over 12")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

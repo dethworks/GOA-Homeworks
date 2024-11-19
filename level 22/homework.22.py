518858aa75c5ba3c22aadecb713e066b28026eed
dethworks <lukashukvani1@gmail.com>
2024-11-19 20:28:35 +0400

    second commit

---
+++ level 22/homework.22.py
@@ -0,0 +1,50 @@
+list_of_names = ["luka","davit","nika","davit"]
+
+
+name_to_count = "davit"
+
+count_of_name = list_of_names.count(name_to_count)
+
+print(f"{count_of_name} times ")
+                           #2
+
+num = [1, 2, 3, 4, 5, 6, 7]
+
+num.reverse()
+
+print(num)
+                           # 3
+
+
+list_of_numbers = [1, 2, 3, 4]
+
+length = len(list_of_numbers)
+
+amount = list_of_numbers * length
+
+print(amount)
+
+                         # 4
+
+insert_arr = ["dog", "cat", "parrot", "monkey"]
+
+
+insert_arr.insert(1, "hippo")
+
+print(insert_arr)
+
+                           # 5
+
+fruits = ["apple", "banana", "orange",]
+
+
+fruits.count("apple")
+
+fruits.remove("apple")
+
+print(fruits)
+
+
+
+
+

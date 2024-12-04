0fb0e3a547b3407ef18bf252a879f2cc4977af11
dethworks <lukashukvani1@gmail.com>
2024-12-04 19:54:16 +0400

    second commit

---
+++ level 26/homework 26.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+def triangle():
+    speed(8)
+    begin_fill()
+    pensize(10)
+    color("blue")
+    forward(107)
+    left(125)
+    forward(99)
+    left(111)
+    forward(99)
+    end_fill()
+
+for i in range(120):
+    triangle()
+
+exitonclick()
+                           #2
+
+def hello_world():
+    print("print()")
+
+hello_world()
+
+
+
+
+
+
+

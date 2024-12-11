dfce897a835389c6da7017213db038596dcdb692
dethworks <lukashukvani1@gmail.com>
2024-12-11 07:22:09 +0400

    second commit

---
+++ homework 28/homework 28.py
@@ -0,0 +1,25 @@
+                                      #1
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+
+                                      #2
+def remove_char(s):
+    return s[1:-1]
+                                      #3
+def string_to_number(s):
+    return int(s)
+                                      #4
+def no_space(x):
+    return x.replace(" ", "")
+                                      #5
+def sum_array(a):
+    return sum(a)
+
+
+
+
+
+
+
+
+

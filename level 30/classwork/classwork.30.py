5b41c270c976a0b0b1cde86b1ad46ac2f01ee765
dethworks <lukashukvani1@gmail.com>
2024-12-17 20:27:50 +0400

    first commit

---
+++ level 30/classwork/classwork.30.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 8, 34]
+
+min_number = min(numbers)
+max_number = max(numbers)
+
+print(min_number, max_number)
---
+++ level 30/homework/homework.30.py
@@ -0,0 +1,16 @@
+                                          #1
+                                          #2
+def square_sum(numbers):
+    return sum(x ** 2 for x in numbers)
+                                          #3
+def sum_numbers(arr):
+    return sum(arr)
+                                          #4
+
+
+
+
+
+
+
+

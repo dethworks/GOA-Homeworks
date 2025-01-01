0bbb5b6c1043c7e93a925d9ea09f22196a3bf548
dethworks <lukashukvani1@gmail.com>
2025-01-01 11:06:33 +0400

    first commit

---
+++ level 34/classwork/classwork.py
@@ -0,0 +1,16 @@
+movies = ("rocky", "rambo", "rocky 2", "gladiator", "Matrix", "The Shawshank Redemption")
+
+first_movie, *other_movies = movies
+
+print(f"1 ფილმი: {first_movie}")
+print(f"დანარჩენი 5 ფილმი: {other_movies}")
+
+
+movies = ("rocky", "rambo", "rocky 2", "gladiator", "Matrix", "The Shawshank Redemption")
+
+
+first_movie = movies[0]
+last_movie = movies[-1]
+
+print(f"პირველი ფილმი: {first_movie}")
+print(f"ბოლო ფილმი: {last_movie}")
---
+++ level 34/homework/all tuples.py
@@ -0,0 +1,34 @@
+                                      #1
+games = ("mafia", "harvester", "grim fandango", "postal")
+
+print(games[:2])
+
+                                      #2
+
+
+numbers = (2, 56, 99, 22, 15, 23, 66, 11, 134, 23, 66, 91, 22, 2, 23)
+
+counted_23 = numbers.count(23)
+
+print(counted_23)
+
+                                    # 3
+
+
+aura = (10, 25, 5, 80, 70, 20)
+
+
+
+for num in aura:
+    if num > 10:
+        print(num)
+
+                                    # 4
+
+# List არის mutable, ანუ მისი შეცვლა შეიძლება.
+# Tuple არის immutable, ანუ მისი შეცვლა შეიძლება.
+
+                                    # 5
+
+# Mutable ნიშნავს შეცვლადს.
+# Immutable ნიშნავს უშეცვლადს

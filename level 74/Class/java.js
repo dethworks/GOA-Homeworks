93a4d0f9509a60d434d8df3685150e350289ef82
dethworks <lukashukvani1@gmail.com>
2025-05-20 20:40:09 +0400

    first commit

---
+++ level 74/Class/java.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 5;
+
+if (a > b) {
+    console.log("a > b");
+}
+
+let boxes = document.querySelectorAll('.box');
+
+for (let i = 0; i < boxes.length; i++) {
+    console.log(boxes[i]);
+}
